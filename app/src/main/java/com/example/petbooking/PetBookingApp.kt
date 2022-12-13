2461a2a5f53280947ffa72c14835db6f9cffbc77
akozlova <alena99.kozlova99@gmail.com>
2022-12-13 13:22:04 +0300

    test ci

--- app/src/main/java/com/example/petbooking/PetBookingApp.kt
+++ app/src/main/java/com/example/petbooking/PetBookingApp.kt
@@ -1,8 +1,6 @@
 package com.example.petbooking
 
 import android.app.Application
-import android.content.Context
-import com.example.petbooking.di.AppComponent
 import com.example.petbooking.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
